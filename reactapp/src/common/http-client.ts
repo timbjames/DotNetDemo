15630ebbd9e1f3af39f5f96be3ffee6fb7c96d4d
timjames <timjames@pingidentity.com>
2021-08-06 16:09:10 +0100

    Adding new stuff

--- reactapp/src/common/http-client.ts
+++ reactapp/src/common/http-client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://api.thecatapi.com/v1',
+  baseURL: 'http://localhost:5000',
   headers: {
     'Content-type': "application/json",
     'x-api-key': process.env.REACT_APP_API_KEY
