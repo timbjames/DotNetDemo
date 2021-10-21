0e6f505f4c24551832c5ae9b1503991d79fbaaa6
timjames <timjames@pingidentity.com>
2021-10-21 11:04:52 +0100

    adding react-flow

--- reactapp/src/pages/people/people.page.tsx
+++ reactapp/src/pages/people/people.page.tsx
@@ -8,8 +8,63 @@ const People: React.FC = () => {
             <h1>People</h1>
 
             <PersonList />
+
+            <ParentComponent />
         </div>
     );
 };
+
+class ParentComponent extends React.Component<{}, {}> {
+
+    private child: React.RefObject<ChildOneComponent>;
+
+    constructor(props: any){
+        super(props);
+
+        this.child = React.createRef();
+    }
+
+    doSomething = () => {
+        this.child.current?.doSomething();
+    }
+
+    render(){
+
+        return (
+            <div>
+                <h1>Parent {Date.now()}</h1>
+                <ChildOneComponent ref={this.child} />
+                <ChildTwoComponent handleClick={this.doSomething} />
+            </div>
+        )
+    }
+};
+
+class ChildOneComponent extends React.Component<{}, {}> {
+
+    public doSomething() {
+        alert('here');
+    }
+
+    render() {
+        return (
+            <div>Child One</div>
+        );
+    }
+}
+
+class ChildTwoComponent extends React.Component<{ handleClick: () => void }, {}> {
+
+
+    render(){
+        return (
+            <div>Child Two
+                <button onClick={() => {
+                    this.props.handleClick();
+                }}>Click This Button</button>
+            </div>
+        );
+    }
+}
 
 export default People;
--- reactapp/src/pages/root.component.tsx
+++ reactapp/src/pages/root.component.tsx
@@ -5,7 +5,15 @@ import People from '@pages/people/people.page';
 import Cars from '@pages/car/car.page';
 import { routes } from '@pages/routes';
 
+import ReactFlow from 'react-flow-renderer';
+
 const PageRoot: React.FC = () => {
+
+    const elements = [
+        {id: '1', type: 'input', data: { label: 'Node 1' }, position: { x: 250, y: 5 }},
+        { id: '2', data: { label: <div>Node 2</div> }, position: { x: 100, y: 100 } },
+        { id: 'e1-2', source: '1', target: '2', animated: true }
+    ];
 
     return (
         <Switch>
@@ -16,7 +24,12 @@ const PageRoot: React.FC = () => {
                 <Cars />
             </Route>
             <Route path={routes.home.url}>
+                <>
                 <h1>Welcome!</h1>
+                <div style={{ backgroundColor: '#ccc', height: 500 }}>
+                    <ReactFlow elements={elements} />
+                </div>
+                </>
             </Route>
         </Switch>
     );
